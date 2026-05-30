cc79233094ca9bf34be4e4ae2680138d3a5b65d3
osint-surce <kotost68@gmail.com>
2026-05-30 21:14:40 +0300

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(["POST"])
+def register(request):
+    username = request.data.get("username")
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if User.objects.filter(username=username).exists():
+        return Response(
+            {"error": "Пользователь уже существует"},
+            status=400
+        )
+
+    User.objects.create_user(
+        username=username,
+        email=email,
+        password=password
+    )
+
+    return Response({
+        "message": "Успешная регистрация"
+    })
