2e3affe427d26b2aa3c808255c64de13730221a2
osint-surce <kotost68@gmail.com>
2026-06-01 16:57:56 +0300

    Update tests.py

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTest(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username="test",
+            password="123456"
+        )
+
+        self.assertEqual(
+            user.username,
+            "test"
+        )
