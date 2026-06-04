c302f4081c4029f2da72f03916435555b56317f4
osint-surce <kotost68@gmail.com>
2026-06-04 23:47:13 +0300

    Update views.py

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.filter(is_active=True)
+    serializer_class = EventSerializer
+    permission_classes = [permissions.AllowAny]  # пока открыто, потом можно ограничить
