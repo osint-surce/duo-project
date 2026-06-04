2358ccae4292ebcabd689faa27656594f762917f
osint-surce <kotost68@gmail.com>
2026-06-04 23:57:13 +0300

    Update views.py

--- backend/events/views.py
+++ backend/events/views.py
@@ -5,4 +5,4 @@ from .serializers import EventSerializer
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.filter(is_active=True)
     serializer_class = EventSerializer
-    permission_classes = [permissions.AllowAny]  # пока открыто, потом можно ограничить
+    permission_classes = [permissions.AllowAny] 
