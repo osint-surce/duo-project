50327fd599bfcc191728c6434a8418b3be7121f2
osint-surce <kotost68@gmail.com>
2026-06-04 22:14:08 +0300

    Update admin.py

--- backend/events/admin.py
+++ backend/events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'date', 'location', 'price', 'is_active')
+    list_filter = ('is_active', 'date')
+    search_fields = ('title', 'artist', 'location')
